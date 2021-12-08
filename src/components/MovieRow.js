934efabaa7ac358492f9ed5a73ae89269ecb8d9c
Carlos <carlos.av.amorim@gmail.com>
2021-12-08 20:24:13 -0300

    Fix API and CSS

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -6,7 +6,7 @@ export default function MovieRow({title, items}) {
         <div className="movieRow">
             <h2>{title}</h2>
             <div className="movieRow--listarea">
-                <div className="movieRow--lista"> {/* Todos os filmes nessa div, mas dentro a div abaixo vai mostrar somente filmes na tela */}
+                <div className="movieRow--list"> {/* Todos os filmes nessa div, mas dentro a div abaixo vai mostrar somente filmes na tela */}
                     {items.results?.length > 0 && items.results.map((item, key)=>(
                         <div key={key} className="movieRow--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
